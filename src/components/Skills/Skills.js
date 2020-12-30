f73d04436ce180f9a66386d7662c8d9dc9c05dcf
Prince Raj Roy <princelikesall16@gmail.com>
2020-12-30 13:56:56 +0530

    Added Skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,7 +2,7 @@ import Skill from './Skill/Skill'
 import './Skills.sass'
 import HTML from '../../utilities/images/HTML.svg'
 import NODE from '../../utilities/images/NODE.svg'
-import FIGMA from '../../utilities/images/FIGMA.svg'
+import FIGMA from '../../utilities/images/FIGMA.png'
 import BLENDER from '../../utilities/images/BLENDER.svg'
 
 function Skills() {
@@ -14,10 +14,10 @@ function Skills() {
                 <div className="line right"></div>
             </div>
             <div className="Skills__bg">
-                <Skill icon={HTML} title={"Front-End Development"} desc="HTML, CSS, JS" subtitle="Frameworks" subdesc="ReactJS, Bootstrap, React-Spring, GSAP"/>
-                <Skill icon={NODE} title={"Back-End Development"} desc="NodeJS, KnexJS" subtitle="Frameworks" subdesc="HapiJS, ExpressJS"/>
-                <Skill icon={FIGMA} title={"Softwares"} desc="FIGMA, VSCODE, Anaconda apps" subtitle="Frameworks" subdesc="ReactJS, Bootstrap, React-Spring"/>
-                <Skill icon={BLENDER} title={"Meta"} desc="Blender, Inkscape, VSDC"/>
+                <Skill icon={HTML} title={"Front-End Development"} desc="HTML, CSS, JS, SASS" subtitle="Some Frameworks" subdesc="ReactJS, Bootstrap, React-Spring, GSAP"/>
+                <Skill icon={NODE} title={"Back-End Development"} desc="NodeJS, KnexJS, ObjectionJs" subtitle="Some Frameworks" subdesc="HapiJS, ExpressJS"/>
+                <Skill icon={FIGMA} title={"Softwares"} desc="FIGMA, VSCode, Xampp, Postman, SQLite, Anaconda Apps, MySQL Workbench" subtitle="Online Platforms" subdesc="Firebase, Heroku"/>
+                <Skill icon={BLENDER} title={"Meta"} desc="C, C++, Java, Python, Matlab, MySQL" subtitle={"Other Softwares"} subdesc={"Blender, Inkscape, VSDC"}/>
             </div>
         </div>
     )
