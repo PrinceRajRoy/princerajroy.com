94186cfde76051962b461f663d5d78c3c72d953b
Prince Raj Roy <princerajroy313@gmail.com>
2021-01-17 16:37:49 +0530

    Minor Fix - Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,7 @@ import './Footer.sass'
 function Footer() {
     return (
         <div className="Footer">
-            <div className="Footer__section">
-                <div className="Footer__text">Prince Raj Roy</div>
-                <div className="Footer__text">7002434736</div>
-                <div className="Footer__text">princerajroy313@gmail.com</div>
-            </div>
-            <div className="Footer__section">
-                <div className="Footer__text">Makum Jn.</div>
-                <div className="Footer__text">Dist - Tinsukia, Assam, 786170</div>
-            </div>
+            Made by Prince Raj Roy
         </div>
     )
 }
